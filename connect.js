1df9595429ca4c88bce93a1de9c485e9fed3bb8f
erikwett <erik.wetterberg@gmail.com>
2019-07-03 10:58:18 +0200

    simplify connect.js

--- connect.js
+++ connect.js
@@ -1,10 +1,8 @@
-window.connect = function connect(host,id) {
-  const loadSchema = () => fetch('https://unpkg.com/enigma.js/schemas/3.2.json').then(response => response.json());
-
-  const createConnection = () => loadSchema().then(schema => window.enigma.create({
-    schema,
-    url: `ws://${host}/app/${id}`,
-  }).open().then(qix => qix.openDoc(id)));
-
-  return createConnection();
+window.connect = function connect(host, id) {
+	return fetch('https://unpkg.com/enigma.js/schemas/3.2.json').
+	then(response => response.json()).
+	then(schema => window.enigma.create({
+		schema,
+		url: `ws://${host}/app/${id}`,
+	}).open().then(qix => qix.openDoc(id)));
 };
